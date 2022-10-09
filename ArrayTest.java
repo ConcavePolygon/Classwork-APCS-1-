081df3efec94e1e6a5ce6aaa49e561aabbb8150e
Stanley Tung <stung40@stuy.edu>
2022-10-08 20:49:15 -0400

    CW for October 3rd: ArrayTest

--- ArrayTest.java
+++ ArrayTest.java
@@ -1,1 +1,19 @@
+public class ArrayTest {
+  public static void main(String[] args){
+    int[] anArray = new int[11];
+    System.out.print("Part 1: ");
+    System.out.print("" + anArray); // Memory address
 
+    System.out.print("\nPart 2: ");
+    for (int i=0; i<anArray.length; i++){
+      anArray[i] = 100 + (10 * i);
+    }
+    System.out.print(anArray[0] + " " + anArray[5] + " " + anArray[10]);
+
+    System.out.print("\nPart 3: ");
+    for (int i=0; i<anArray.length; i++){
+      System.out.print(anArray[i] + " ");
+    }
+
+  }
+}
